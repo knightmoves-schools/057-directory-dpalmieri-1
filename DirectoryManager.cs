cbefe6277573081ed14c22e1a0e43616e934d767
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-03-30 17:18:52 +0000

    WriteAllText

--- DirectoryManager.cs
+++ DirectoryManager.cs
@@ -7,5 +7,6 @@ public class DirectoryManager{
     public void Create(string directoryName){
         // Add your code here
         Directory.CreateDirectory(directoryName + "test-directory");
+        File.WriteAllText(directoryName, "test-directory");
     }
 }
