0969118b1dead49a7d957f5884dcacdf1fc935c3
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-03-30 17:48:44 +0000

    Convoluted way of getting a directory

--- DirectoryManager.cs
+++ DirectoryManager.cs
@@ -8,5 +8,9 @@ public class DirectoryManager{
         // Add your code here
         string classic = Path.GetTempPath() + directoryName;
         Directory.CreateDirectory(classic + "/test-directory");
+        File.WriteAllText(classic + "/test-directory/test-file.txt", "This is a test file.");
+
+        bool FirstDirectory = Directory.Exists(classic + "/test-directory");
+        bool FirstFile = File.Exists(classic + "/test-directory/test-file.txt");
     }
 }
