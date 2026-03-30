ef30dd81815a026e1030e8dd76a0888cefe465fa
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-03-30 17:54:22 +0000

    Very convoluted

--- DirectoryManager.cs
+++ DirectoryManager.cs
@@ -4,13 +4,13 @@ namespace knightmoves;
 
 public class DirectoryManager{
 
-    public void Create(string directoryName){
+    public bool Create(string directoryName){
         // Add your code here
         string classic = Path.GetTempPath() + directoryName;
         Directory.CreateDirectory(classic + "/test-directory");
         File.WriteAllText(classic + "/test-directory/test-file.txt", "This is a test file.");
-
-        bool FirstDirectory = Directory.Exists(classic + "/test-directory");
-        bool FirstFile = File.Exists(classic + "/test-directory/test-file.txt");
+        bool DirectoryExists = Directory.Exists(classic + "/test-directory");
+        bool FileExists = File.Exists(classic + "/test-directory/test-file.txt");
+        return DirectoryExists && FileExists;
     }
 }
