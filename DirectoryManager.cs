19e2e0be3afb4830102d7914e025f479767c9a39
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-03-31 09:07:13 -0500

    Update DirectoryManager.cs

--- DirectoryManager.cs
+++ DirectoryManager.cs
@@ -9,8 +9,7 @@ public class DirectoryManager{
         string classic = Path.GetTempPath() + directoryName;
         Directory.CreateDirectory(classic + "/test-directory");
         File.WriteAllText(classic + "/test-directory/test-file.txt", "This is a test file.");
-        bool DirectoryExists = Directory.Exists(classic + "/test-directory");
-        bool FileExists = File.Exists(classic + "/test-directory/test-file.txt");
-        return DirectoryExists && FileExists;
+        
+        return Directory.Exists(classic + "/test-directory");
     }
 }
