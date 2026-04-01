0cdd748dcef02b6bb1616a00f8ab8fc103414a20
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-04-01 13:51:19 -0500

    Update DirectoryManager.cs

--- DirectoryManager.cs
+++ DirectoryManager.cs
@@ -1,15 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-
 namespace knightmoves;
 
 public class DirectoryManager{
 
-    public bool Create(string directoryName){
+    public void Create(string directoryName){
         // Add your code here
-        string classic = Path.GetTempPath() + directoryName;
-        Directory.CreateDirectory(classic + "/test-directory");
-        File.WriteAllText(classic + "/test-directory/test-file.txt", "This is a test file.");
+        string classic = Path.Combine(directoryName, "test-directory");
 
-        return File.Exists(classic);
+        Directory.CreateDirectory(classic);
     }
 }
